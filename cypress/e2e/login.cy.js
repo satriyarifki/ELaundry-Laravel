41392a29e0326f7f8b1197f32e77ce88d6bc6b3c
satriyarp <rifkisatriya@gmail.com>
2022-12-16 13:33:39 +0700

    Add Docker Config

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,16 +1,16 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('/')
-    cy.get('[name="email"]').type("admin@gmail.com")
-    cy.get('[name="password"]').type("password")
-    cy.get('[name="login-button"]').click()
+describe("empty spec", () => {
+    it("passes", () => {
+        cy.visit("/");
+        cy.get('[name="email"]').type("admin@gmail.com");
+        cy.get('[name="password"]').type("password");
+        cy.get('[name="login-button"]').click();
 
-    cy.get('[data-id="sidebar-cust"]').click()
-    cy.get('[data-id="sidebar-exp"]').click()
-    cy.get('[data-id="sidebar-ext"]').click()
-    cy.get('[data-id="sidebar-tran"]').click()
+        cy.get('[data-id="sidebar-cust"]').click();
+        cy.get('[data-id="sidebar-exp"]').click();
+        cy.get('[data-id="sidebar-ext"]').click();
+        cy.get('[data-id="sidebar-tran"]').click();
 
-    cy.get('[data-id="app-nav"]').click()
-    cy.get('[data-id="logout"]').click()
-  })
-})
+        cy.get('[data-id="app-nav"]').click();
+        cy.get('[data-id="logout"]').click();
+    });
+});
